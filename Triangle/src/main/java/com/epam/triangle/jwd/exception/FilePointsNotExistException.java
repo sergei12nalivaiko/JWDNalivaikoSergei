d89ec261aeb598e740e763f4cdabc9ec2b58ff97
Sergei Nalivaiko <sergei12nalivaiko@gmail.com>
2021-08-14 17:37:10 +0300

    delete triangle

--- Triangle/src/main/java/com/epam/triangle/jwd/exception/FilePointsNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.triangle.jwd.exception;
-
-public class FilePointsNotExistException extends Exception{
-    public FilePointsNotExistException(String message) {
-        super(message);
-    }
-}
